3eb340ded515c893ef1e74a45093b8052027c87d
Wong Jia Hau <hou32hou@gmail.com>
2018-07-18 11:42:49 +0800

    Change tab to spaces

--- main.py
+++ main.py
@@ -8,118 +8,118 @@ DB = 'db.sqlite'
 
 def parseUser(row):
     return {
-        'id': 		row[0],
-        'email': 	row[1],
+        'id':         row[0],
+        'email':     row[1],
     }
-	
+    
 def parseTask(row):
     return {
-        'id': 		row[0],
-        'user_id': 	row[1],
-		'title': 	row[2],
-		'content': 	row[3],
-		'pinned': 	row[4],
+        'id':         row[0],
+        'user_id':     row[1],
+        'title':     row[2],
+        'content':     row[3],
+        'pinned':     row[4],
     }
 
 def parseReminder(row):
     return {
-        'id': 		row[0],
-        'task_id': 	row[1],
-		'date': 	row[2],
+        'id':         row[0],
+        'task_id':     row[1],
+        'date':     row[2],
     }
-	
+    
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-	return """<p>It works!</p>"""
+    return """<p>It works!</p>"""
 
 
 @app.route('/api/admin/see_table/<string:table>', methods=['GET'])
 def admin_get_table(table):
-	parser = {
-		'user': 	parseUser,
-		'task': 	parseTask,
-		'reminder':	parseReminder
-	}
-	data = fetchData(parser[table], f'SELECT * FROM {table}')
-	return jsonify(data), 200
+    parser = {
+        'user':     parseUser,
+        'task':     parseTask,
+        'reminder':    parseReminder
+    }
+    data = fetchData(parser[table], f'SELECT * FROM {table}')
+    return jsonify(data), 200
 
-	
+    
 @app.route('/api/get_task/<string:email>', methods=['GET'])
 def get_task(email):
-	user_id = fetch_user_id(email)
-	tasks = fetch_task(user_id)
-	for task in tasks:
-		task["reminder"] = fetch_reminder(task["id"])
-	return jsonify(tasks), 200
+    user_id = fetch_user_id(email)
+    tasks = fetch_task(user_id)
+    for task in tasks:
+        task["reminder"] = fetch_reminder(task["id"])
+    return jsonify(tasks), 200
 
 def fetch_reminder(task_id):
-	return fetchData(parseReminder, 
-	"""
-	SELECT * FROM reminder
-	WHERE task_id = ?
-	""", (task_id,))
-	
+    return fetchData(parseReminder, 
+    """
+    SELECT * FROM reminder
+    WHERE task_id = ?
+    """, (task_id,))
+    
 def fetch_task(user_id):
-	return fetchData(parseTask, 
-	"""
-	SELECT * FROM task
-	WHERE user_id = ?
-	""", (user_id,))
+    return fetchData(parseTask, 
+    """
+    SELECT * FROM task
+    WHERE user_id = ?
+    """, (user_id,))
 
 def fetch_user_id(email):
-	return fetchData(parseUser, 
-	"""
-	SELECT * FROM user
-	WHERE email = ?
-	""", (email,))[0]["id"]
-	
+    return fetchData(parseUser, 
+    """
+    SELECT * FROM user
+    WHERE email = ?
+    """, (email,))[0]["id"]
+    
 def fetchData(parser, query, queryParam=None):
-	db = sqlite3.connect(DB)
-	cursor = db.cursor()
-	if queryParam:
-		cursor.execute(query, queryParam)
-	else:
-		cursor.execute(query)
-	rows = cursor.fetchall()
-	db.close()
-	result = []
-	for row in rows:
-		result.append(parser(row))
-	return result
+    db = sqlite3.connect(DB)
+    cursor = db.cursor()
+    if queryParam:
+        cursor.execute(query, queryParam)
+    else:
+        cursor.execute(query)
+    rows = cursor.fetchall()
+    db.close()
+    result = []
+    for row in rows:
+        result.append(parser(row))
+    return result
 
 def insertData(query, queryParam):
-	db = sqlite3.connect(DB)
-	cursor = db.cursor()
-	cursor.execute(query, queryParam)
-	id = cursor.lastrowid
-	db.commit()
-	response = {
-		'id': id,
-		'affected': db.total_changes,
-	}
-	db.close()
-	return response
-	
+    db = sqlite3.connect(DB)
+    cursor = db.cursor()
+    cursor.execute(query, queryParam)
+    id = cursor.lastrowid
+    db.commit()
+    response = {
+        'id': id,
+        'affected': db.total_changes,
+    }
+    db.close()
+    return response
+    
 
 @app.route('/api/new_task', methods=['POST'])
 def store():
-	if not request.json:
-		abort(404)
-		
-	new_task = (
+    if not request.json:
+        abort(404)
+        
+    new_task = (
         request.json['user_id'],
         request.json['title'],
         request.json['content'],
         request.json['pinned'],
-	)
-	response = insertData("""
+    )
+    response = insertData("""
         INSERT INTO task (user_id,title,content,pinned)
         VALUES(?,?,?,?)
     """, new_task)
 
-	return jsonify(response), 200
+    return jsonify(response), 200
 
 
 @app.route('/api/places/<int:place>', methods=['PUT'])
